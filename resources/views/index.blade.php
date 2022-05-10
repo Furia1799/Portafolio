1f324b324b2df3d555b73dfbc0f9a1b4a3a322fe
Bryan Reyes <furia1799@gmail.com>
2022-05-09 21:06:36 -0500

    primer commit de interface

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -161,6 +161,7 @@
                 <div class="col">Location</div>
                 <div class="col">aroun the web</div>
                 <div class="col">about me</div>
+                
             </div>
         </div>
     </footer>
